7af495b35528d2027e9cc55c451c641bc1b9daf2
colesmith <colesmith@ls-imac.lan>
2025-07-13 23:49:20 -0400

    Janr03.java
    o Cleanup

--- src/janr2003_java/src/main/java/org/clas/lib/Janr03.java
+++ src/janr2003_java/src/main/java/org/clas/lib/Janr03.java
@@ -4,23 +4,27 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class Janr03 extends Constants {
 	
-    public static void main(String[] args) { };
-    
-    
+	Scanner filescanner;
+    String infile   = janrPath+"janr_input_e1b";
+    String resfile  = janrPath+"res-q0.4.inp";
+    String parifile = null, resifile=null;
+    JanrInputRes  jir = new JanrInputRes();
+
+    public Janr03() {
+    	janr_main();
+    }
+        
     public void janr_main() {
 
         janrInit();
         
-        System.out.println("JANR_MAIN: Begin reading tables");
-
+        System.out.println("Janr03.janr_main(): Begin reading tables");
         readMultables();
         reggeMultables();
-        highMultables();
-        
-        System.out.println("JANR_MAIN: End reading tables");
+        highMultables();        
+        System.out.println("Janr03.janr_main(): End reading tables");
         
         if (fitopt.equals("F")) {
             janrFit();
@@ -32,92 +36,92 @@ public class Janr03 extends Constants {
             janrOutputMpole();
         }	
     }
+    
 
     public void janrInit() {
     	
     	double qavg;
-    	String resifile;
-    	String parifile;
-   	
-    	try (Scanner sc = new Scanner(System.in)) {
+    	 	
+        System.out.println("Janr03.janrInit()");
+        
+        janrInFile(infile);
+        jir = new JanrInputRes();                jir.loadResonanceData(resfile);
+        janrParFile(parifile); 
+        JanrInputSAID jis = new JanrInputSAID(); jis.loadMultipoles();
 
-          String infile = sc.nextLine().trim();
+        if (grfopt != "N") hjanrInit(fname);                
 
-          System.out.println("JANR_INIT: Reading the input file " + infile);
-
-                try (Scanner fileScanner = new Scanner(new File(infile))) {
-                    fitopt = fileScanner.next();
-                    grfopt = fileScanner.next();
-                    n1 = fileScanner.nextInt();
-                    n2 = fileScanner.nextInt();
-                    nebac = fileScanner.nextInt();
-                    obsv2fit = fileScanner.nextInt();
-                    ebeam = fileScanner.nextFloat();
-                    wmin = fileScanner.nextFloat();
-                    wmax = fileScanner.nextFloat();
-                    wstep = fileScanner.nextFloat();
-                    q2min = fileScanner.nextFloat();
-                    q2max = fileScanner.nextFloat();
-                    q2step = fileScanner.nextFloat();
-                    cosmin = fileScanner.nextFloat();
-                    cosmax = fileScanner.nextFloat();
-                    cosstep = fileScanner.nextFloat();
-                    phimin= fileScanner.nextFloat();
-                    phimax = fileScanner.nextFloat();
-                    phistep = fileScanner.nextFloat();
-                    IntAccur = fileScanner.nextFloat();
-                    inffile = fileScanner.next();
-                    maxcalls = fileScanner.nextInt();
-                    tolorance = fileScanner.nextFloat();
-                    errorup = fileScanner.nextFloat();
-                    parms = fileScanner.nextInt();
-                    resifile = fileScanner.next();
-                    parifile = fileScanner.next();
-                    parofile = fileScanner.next();
-                    datform = fileScanner.nextInt();
-                    lmini = fileScanner.nextInt();
-                    lmino = fileScanner.nextInt();
-                    lminf = fileScanner.nextInt();
-                    ChiMax = fileScanner.nextFloat();
-                    for (int i = 0; i < 4; i++) fname[i] = fileScanner.next(); 
-                }
-
-                JanrInputRes loader1 = new JanrInputRes(resifile);
-                loader1.loadResonanceData();
-
-                try (Scanner parScanner = new Scanner(new File(parifile))) {
-                    if (parms > 0 && parms <= MAXpar) {
-                        for (int i = 0; i < parms; i++) {
-                            plist[i] = parScanner.nextInt();
-                            pname[i] = parScanner.next();
-                            start_value[i] = parScanner.nextFloat();
-                            step_size[i] = parScanner.nextFloat();
-                            par_stat[i] = parScanner.nextInt();
-                            low_lim[i] = parScanner.nextFloat();
-                            up_lim[i] = parScanner.nextFloat();
-                        }
-                    } else {
-                        System.out.println("JANR_INIT: Wrong Number of parameters=" + parms);
-                        return;
-                    }
-                }
-
-                JanrInputSAID loader2 = new JanrInputSAID();
-                loader2.loadMultipoleData();
-
-                if (grfopt != "N") hjanrInit(fname);                
-
-                qavg = 0.5f * (q2min + q2max);
-                
-                JanrIniPoint ini = new JanrIniPoint(qavg);
-                MultAna       ma = new MultAna(qavg); 
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
+        qavg = 0.5f * (q2min + q2max);                
+        JanrIniPoint ini = new JanrIniPoint(qavg);
+        
+        MultAna       ma = new MultAna(); ma.multAna(qavg);
 
     }
+    
+    public void janrInFile(String file) {
+        System.out.println("janrInFile("+file+")");
+        try (Scanner fileScanner = new Scanner(new File(file))) {
+            fitopt = fileScanner.next();  
+            grfopt = fileScanner.next();  
+            n1 = fileScanner.nextInt();  
+            n2 = fileScanner.nextInt();  
+            nebac = fileScanner.nextInt();  
+            obsv2fit = fileScanner.nextInt();
+            ebeam = fileScanner.nextFloat();
+            wmin = fileScanner.nextFloat();
+            wmax = fileScanner.nextFloat();
+            wstep = fileScanner.nextFloat();
+            q2min = fileScanner.nextFloat();
+            q2max = fileScanner.nextFloat();
+            q2step = fileScanner.nextFloat();
+            cosmin = fileScanner.nextFloat();
+            cosmax = fileScanner.nextFloat();
+            cosstep = fileScanner.nextFloat();
+            phimin= fileScanner.nextFloat();
+            phimax = fileScanner.nextFloat();
+            phistep = fileScanner.nextFloat();
+            IntAccur = fileScanner.nextFloat();
+            inffile = fileScanner.next();
+            maxcalls = fileScanner.nextInt();
+            tolorance = fileScanner.nextFloat();
+            errorup = fileScanner.nextFloat();
+            parms = fileScanner.nextInt();
+            resifile = janrPath+fileScanner.next();
+            parifile = janrPath+fileScanner.next();
+            parofile = fileScanner.next();
+            datform = fileScanner.nextInt();
+            lmini = fileScanner.nextInt();
+            lmino = fileScanner.nextInt();
+            lminf = fileScanner.nextInt();
+            ChiMax = fileScanner.nextFloat(); 
+            for (int i = 0; i < 4; i++) fname[i] = fileScanner.next(); 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }    	    	
+    }
+    
+    public void janrParFile(String file) {
+        System.out.println("janrParFile("+file+")");
+    	try (Scanner parScanner = new Scanner(new File(file))) {
+            if (parms > 0 && parms <= MAXpar) {
+                for (int i = 0; i < parms; i++) {
+                    plist[i] = parScanner.nextInt();
+                    pname[i] = parScanner.next();
+                    start_value[i] = parScanner.nextFloat();
+                    step_size[i] = parScanner.nextFloat();
+                    par_stat[i] = parScanner.nextInt();
+                    low_lim[i] = parScanner.nextFloat();
+                    up_lim[i] = parScanner.nextFloat();
+                }
+            } else {
+                System.out.println("janrParFile: Wrong Number of parameters=" + parms);
+                return;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }    	
+    }
+
 
 
     private static void readMultables() {
@@ -147,7 +151,11 @@ public class Janr03 extends Constants {
     private static void hjanrInit(String[] fname) {
     	
     }
- 
+    	
+    public static void main(String[] args) { 
+    	Janr03 j = new Janr03();
+    };
+     
 }
 
 
