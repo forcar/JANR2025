5478f4e5fc7c501abe942a7c48e1211fc61de4f2
colesmith <colesmith@imac.lan>
2025-09-25 17:38:56 -0400

    JanrMonitor.java
    o Add Reset button to restore default fit
    o Use GridBagLayout and FontMetrics for COF slider label panel to
    eliminate slider jitter when slider label is resizing.

--- src/janr2003_java/src/main/java/org/clas/lib/JanrMonitor.java
+++ src/janr2003_java/src/main/java/org/clas/lib/JanrMonitor.java
@@ -3,8 +3,10 @@ package org.clas.lib;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.FontMetrics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -36,7 +38,8 @@ public class JanrMonitor implements ActionListener {
     private EmbeddedCanvasTabbed             fitc = new EmbeddedCanvasTabbed("FITC");
 
     private String                   detectorName = null;
-    private JPanel                  detectorPanel = null;
+    private JPanel                  detectorPanel = new JPanel();
+    
     private EmbeddedCanvasTabbed[] detectorCanvas = new EmbeddedCanvasTabbed[2];
     private ArrayList<String>[]  detectorTabNames = (ArrayList<String>[])new ArrayList[2];
     public  DataGroupManager[]                dgm = new DataGroupManager[2];
@@ -53,8 +56,9 @@ public class JanrMonitor implements ActionListener {
     private JPanel                  actionPanel = null;
     private JPanel               controlsPanel0 = null;
     private JPanel               controlsPanel1 = null;
-    private JPanel               controlsPanel2 = null;
  
+	private GridBagConstraints              gbc = new GridBagConstraints();
+
     public Boolean                  histosExist = false;   
     public String                          root = " ";
     public JSlider                    binslider = null;
@@ -62,13 +66,14 @@ public class JanrMonitor implements ActionListener {
     private ButtonGroup bG0, bG1;
     private JRadioButton bW,bC,bF,b1,b2,b3,b4;
     private Boolean useWCF=false, use1234=false, useBSP=false;
-    private JLabel cof_resonance = new JLabel();
+
+    private JLabel  cof_resonance = new JLabel();
     private JLabel cof_parameters = new JLabel();
+    private JLabel      cof_label = new JLabel("" + String.format("%d", 0)); 
     
     public JanrMonitor(String name) {
 		initGStyle(14);
-	    detectorName   = name;
-	    detectorPanel  = new JPanel();	    
+	    detectorName   = name;	    
         root           = detectorName+".JanrMonitor.";	
     	for (int i=0; i<2; i++) detectorTabNames[i] = new ArrayList<String>();
     	for (int i=0; i<2; i++) dgm[i] = new DataGroupManager();
@@ -117,32 +122,33 @@ public class JanrMonitor implements ActionListener {
      }
     
     public void initPanel() {
-        getDetectorPanel().setLayout(new BorderLayout());
+        detectorPanel.setLayout(new BorderLayout());
         actionPanel    = new JPanel(new FlowLayout());
         controlsPanel0 = new JPanel(new GridBagLayout());        
         controlsPanel1 = new JPanel();
+               
+        gbc.fill = GridBagConstraints.HORIZONTAL; 
+        gbc.weightx = 0.5; gbc.gridx=0 ; gbc.gridy=0 ;        
+        controlsPanel0.add(controlsPanel1,gbc);
         
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL; 
-        c.weightx = 0.5; c.gridx=0 ; c.gridy=0 ; 
-        
-        controlsPanel0.add(controlsPanel1,c);
         controlsPanel1.setBorder(BorderFactory.createTitledBorder("Bins"));		       
-        controlsPanel1.add(packActionPanel());
+        controlsPanel1.add(packWCFPanel());
         controlsPanel1.add(packBinSliderPanel());		       
         controlsPanel1.add(packComboBox());
         controlsPanel1.add(packCofResonance());
         controlsPanel1.add(packCofSliderPanel());
+        controlsPanel1.add(packCofSliderLabelPanel());
         controlsPanel1.add(packCofParameters());
+        controlsPanel1.add(packCofParametersLabel());
         controlsPanel1.add(packResetButton());
 
 //        getDetectorPanel().add(getPanel(data,fitc),BorderLayout.CENTER);           
-        getDetectorPanel().add(getPanel(getDetectorCanvas(0),getDetectorCanvas(1)),BorderLayout.CENTER);           
+        detectorPanel.add(getPanel(getDetectorCanvas(0),getDetectorCanvas(1)),BorderLayout.CENTER);  //DATA, FITC canvases         
 //        getDetectorPanel().add(getDetectorCanvas,BorderLayout.CENTER);           
-        getDetectorPanel().add(controlsPanel0,     BorderLayout.SOUTH); 
+        detectorPanel.add(controlsPanel0,     BorderLayout.SOUTH); 
     }
     
-    public JPanel packActionPanel() {
+    public JPanel packWCFPanel() {
     	if (useWCF || use1234) actionPanel.add(getWCFButtonPanel());
     	return actionPanel;
     }
@@ -167,8 +173,18 @@ public class JanrMonitor implements ActionListener {
     	return actionPanel;
     }
     
+    public JPanel packCofSliderLabelPanel() {
+     	actionPanel.add(getCofSliderLabelPanel());
+    	return actionPanel;
+    } 
+    
     public JPanel packCofParameters() {
     	actionPanel.add(getCofParameters());
+    	return actionPanel;
+    } 
+    
+    public JPanel packCofParametersLabel() {
+    	actionPanel.add(getCofParmametersLabel());
     	return actionPanel;
     } 
     
@@ -210,88 +226,24 @@ public class JanrMonitor implements ActionListener {
     }
     
     public JPanel getBinSliderPanel() {
-        JPanel sliderPane1 = new JPanel();
+        JPanel sliderPane = new JPanel();
         JLabel       label = new JLabel("" + String.format("%d", 0));       
         binslider = new JSlider(JSlider.HORIZONTAL, 0, 30, 10); 
         binslider.setPreferredSize(new Dimension(100,10));
-        sliderPane1.add(new JLabel("WCF bin",JLabel.CENTER));
-        sliderPane1.add(binslider);
-        sliderPane1.add(label);  
+        sliderPane.add(new JLabel("WCF bin",JLabel.CENTER));
+        sliderPane.add(binslider);
+        sliderPane.add(label);  
         binslider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e) {            	
                 JSlider slider1 = (JSlider) e.getSource(); 
-                bin = slider1.getValue();                 
+                bin = slider1.getValue();           
                 label.setText(String.valueOf(""+String.format("%d", bin)));
                 plotHistos(getRunNumber(), nt, bin);
             }
         });         
-        return sliderPane1;
-    } 
-    
-    public JPanel getCofSliderPanel() {
-        JPanel sliderPane2 = new JPanel();
-        JLabel      label = new JLabel("" + String.format("%d", 0));              
-        JSlider cofslider = new JSlider(JSlider.HORIZONTAL, -50, 50, 1); 
-
-        cofslider.addChangeListener(new ChangeListener() {
-            public void stateChanged(ChangeEvent e) {
-                JSlider slider2 = (JSlider) e.getSource(); 
-                j.xnew[cof] = slider2.getValue()*0.1f;                 
-                label.setText(String.valueOf(""+String.format("%.2f", j.xnew[cof])));
-                Res2Amp(j.res_name[cof]);
-                plotHistos(getRunNumber(), nt, bin);
-            }
-        });    
-        cofslider.setValue((int)j.xnew[0]);
-        sliderPane2.add(cofslider);
-        sliderPane2.add(label); 
-
-        return sliderPane2;
+        return sliderPane;
     }
-    
-    public JButton getResetButton() {
-        JButton button = new JButton("Reset");
-        button.addActionListener(this); 
-        return button;
-    }
-    
-    public JLabel getCofParameters() {
-        return cof_parameters;
-    }
-    
-    public JLabel getCofResonance() {
-        return cof_resonance;
-    }
-    
-    public void Res2Amp(String res) {
-    	double am=0, ae=0, as=0, a32=0, a12=0, s12=0, rem=0, rsm=0;
-    	switch (res) {
-    	case "P33(1232)": am=j.am3[0];    ae=j.ae3[0];    as=j.as3[0]; rem=ae/am; rsm=as/am; break;
-    	case "P11(1440)": am=j.am1[0][0]; ae=0;           as=j.as1[0][0]; a32=0;                a12=j.aa1[21]*j.cmp1; s12=j.sa1[21]*j.csp1; break;
-    	case "S11(1535)": am=0;           ae=j.ae1[0][1]; as=j.as1[0][1]; a32=0;                a12=j.aa1[22]*j.cep2; s12=j.sa1[22]*j.csp2; break;
-    	case "D13(1520)": am=j.am1[0][2]; ae=j.ae1[0][2]; as=j.as1[0][2]; a32=j.aa3[23]*j.cep3; a12=j.aa1[23]*j.cmp3; s12=j.sa1[23]*j.csp3; break;
-    	case "S11(1650)": am=0;           ae=j.ae1[0][4]; as=j.as1[0][4]; a32=0;                a12=j.aa1[25]*j.cep4; s12=j.sa1[25]*j.csp4; break;
-    	case "S31(1620)": am=0;           ae=j.ae3[4];    as=j.as3[4];    a32=0;                a12=j.aa1[ 5]*j.cep5; s12=j.sa1[ 5]*j.csp5; break;
-    	case "F15(1680)": am=j.am1[0][5]; ae=j.ae1[0][5]; as=j.as1[0][5]; a32=j.aa3[26]*j.cep6; a12=j.aa1[26]*j.cmp6; s12=j.sa1[26]*j.csp6; break;
-    	case "D33(1700)": am=j.am3[1];    ae=j.ae3[1];    as=j.as3[1];    a32=j.aa3[ 2]*j.cep7; a12=j.aa1[ 2]*j.cmp7; s12=j.sa1[ 2]*j.csp7; break;
-    	case "P13(1720)": am=j.am1[0][6]; ae=j.ae1[0][6]; as=j.as1[0][6]; a32=j.aa3[27]*j.cep8; a12=j.aa1[27]*j.cmp8; s12=j.sa1[27]*j.csp8; break;
-    	case "D15(1675)": am=j.am1[0][8]; ae=j.ae1[0][8]; as=0;           a32=j.aa3[29]*j.cep9; a12=j.aa1[29]*j.cmp9; s12=0;                break;
-    	case "F35(1905)": am=j.am3[5];    ae=j.ae3[5];    as=0;           a32=j.aa3[ 6]*j.cep10;a12=j.aa1[ 6]*j.cmp10;s12=0;                break;
-    	case "F37(1950)": am=j.am3[2];    ae=j.ae3[2];    as=0;           a32=j.aa3[ 3]*j.cep11;a12=j.aa1[ 3]*j.cmp11;s12=0; 
-    	}
-    	double[] dcof = {am,ae,as,a32,a12,s12}; String[] scof = new String[6];
-    	int n=0; for (double d : dcof) scof[n++] = String.valueOf(""+String.format("%.2f", d));
-    	setCofParameters(scof);
-    }
-    
-    public void setCofParameters(String[] val) {
-        cof_parameters.setText("<html>"+
-                "<b>MULTIPOLE:</b>&nbsp;AM="+val[0]+"&nbsp;&nbsp;&nbsp;&nbsp;AE="+val[1]+"&nbsp;&nbsp;AS="+val[2]+"<br>" +
-                "<b>ISOSPIN:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A32="+val[3]+"&nbsp;A12="+val[4]+"&nbsp;S12="+val[5]+"<br>" +
-                "</html>"
-            );
-    }
-    
+   
     public JPanel getComboBox() {
 		JPanel sliderParm = new JPanel();
 		sliderParm.setLayout(new FlowLayout());      
@@ -313,7 +265,107 @@ public class JanrMonitor implements ActionListener {
 		
 		return sliderParm;
     }
+
+    public JPanel getCofSliderPanel() {
+        JPanel sliderPanel = new JPanel(new GridBagLayout());                     
+        JSlider  cofslider = new JSlider(JSlider.HORIZONTAL,10, 100, 50); 
+        cofslider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e) {                                 			
+                j.xnew[cof] = cofslider.getValue()*0.1f-5f;  
+                cof_label.setText(String.valueOf(""+String.format("%.2f", j.xnew[cof])));
+                hjanr_loadpar(2);
+                Res2Amp(j.res_name[cof]);
+                plotHistos(getRunNumber(), nt, bin);
+            }
+        });    
+        cofslider.setValue((int)j.xnew[0]); //default for startup
+        gbc.gridx=0; gbc.gridy=0;
+        sliderPanel.add(cofslider,gbc);
+        return sliderPanel;
+    }
     
+    public JPanel getCofSliderLabelPanel() {
+    	JPanel labelPanel = new JPanel(new GridBagLayout());
+    	
+        // Get the widest possible text (to avoid slider jitter when sign changes)
+        String widestText = "-4.00";
+        FontMetrics fm = cof_label.getFontMetrics(cof_label.getFont());
+        int widestWidth = fm.stringWidth(widestText);
+        int widestHeight = fm.getHeight();
+        
+        // Set the preferred size to accommodate the widest string
+        Dimension preferredSize = new Dimension(widestWidth, widestHeight);
+        cof_label.setPreferredSize(preferredSize);
+        
+        gbc.insets = new Insets(10,10,10,10);
+        gbc.gridx=1; gbc.gridy=0;
+    	labelPanel.add(cof_label,gbc);
+    	return labelPanel;
+    }
+    
+    public JPanel getCofParmametersLabel() {
+    	JPanel labelPanel = new JPanel(new GridBagLayout());
+    	
+        // Get the widest possible text (to avoid slider jitter when sign changes)
+        String widestText = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+        FontMetrics fm = cof_parameters.getFontMetrics(cof_parameters.getFont());
+        int widestWidth = fm.stringWidth(widestText);
+        int widestHeight = 2*fm.getHeight();
+        
+        // Set the preferred size to accommodate the widest string
+        Dimension preferredSize = new Dimension(widestWidth, widestHeight);
+        cof_parameters.setPreferredSize(preferredSize);
+        
+        gbc.insets = new Insets(1,1,1,1);
+        gbc.gridx=1; gbc.gridy=0;
+    	labelPanel.add(cof_parameters,gbc);
+    	return labelPanel;
+    }
+        
+    public JLabel getCofParameters() {
+        return cof_parameters;
+    }
+    
+    public JLabel getCofResonance() {
+        return cof_resonance;
+    }
+    
+    public void setCofParameters(String[] val) {
+        cof_parameters.setText("<html>"+
+                "<b>MULTIPOLE:</b>&nbsp;AM="+val[0]+"&nbsp;&nbsp;&nbsp;&nbsp;AE="+val[1]+"&nbsp;&nbsp;AS="+val[2]+"<br>" +
+                "<b>ISOSPIN:</b>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A32="+val[3]+"&nbsp;A12="+val[4]+"&nbsp;S12="+val[5]+"<br>" +
+                "</html>"
+            );
+    }
+
+    public void Res2Amp(String res) {
+    	double am=0, ae=0, as=0, a32=0, a12=0, s12=0, rem=0, rsm=0;
+    	hjanr_loadpar(2); j.jip.janrIniPoint(0.4);
+    	switch (res) {
+    	case "P33(1232)": am=j.am3[0];    ae=j.ae3[0];    as=j.as3[0]; rem=ae/am; rsm=as/am; break;
+    	case "P11(1440)": am=j.am1[0][0]; ae=0;           as=j.as1[0][0]; a32=0;                a12=j.aa1[21]*j.cmp1; s12=j.sa1[21]*j.csp1; break;
+    	case "S11(1535)": am=0;           ae=j.ae1[0][1]; as=j.as1[0][1]; a32=0;                a12=j.aa1[22]*j.cep2; s12=j.sa1[22]*j.csp2; break;
+    	case "D13(1520)": am=j.am1[0][2]; ae=j.ae1[0][2]; as=j.as1[0][2]; a32=j.aa3[23]*j.cep3; a12=j.aa1[23]*j.cmp3; s12=j.sa1[23]*j.csp3; break;
+    	case "S11(1650)": am=0;           ae=j.ae1[0][4]; as=j.as1[0][4]; a32=0;                a12=j.aa1[25]*j.cep4; s12=j.sa1[25]*j.csp4; break;
+    	case "S31(1620)": am=0;           ae=j.ae3[4];    as=j.as3[4];    a32=0;                a12=j.aa1[ 5]*j.cep5; s12=j.sa1[ 5]*j.csp5; break;
+    	case "F15(1680)": am=j.am1[0][5]; ae=j.ae1[0][5]; as=j.as1[0][5]; a32=j.aa3[26]*j.cep6; a12=j.aa1[26]*j.cmp6; s12=j.sa1[26]*j.csp6; break;
+    	case "D33(1700)": am=j.am3[1];    ae=j.ae3[1];    as=j.as3[1];    a32=j.aa3[ 2]*j.cep7; a12=j.aa1[ 2]*j.cmp7; s12=j.sa1[ 2]*j.csp7; break;
+    	case "P13(1720)": am=j.am1[0][6]; ae=j.ae1[0][6]; as=j.as1[0][6]; a32=j.aa3[27]*j.cep8; a12=j.aa1[27]*j.cmp8; s12=j.sa1[27]*j.csp8; break;
+    	case "D15(1675)": am=j.am1[0][8]; ae=j.ae1[0][8]; as=0;           a32=j.aa3[29]*j.cep9; a12=j.aa1[29]*j.cmp9; s12=0;                break;
+    	case "F35(1905)": am=j.am3[5];    ae=j.ae3[5];    as=0;           a32=j.aa3[ 6]*j.cep10;a12=j.aa1[ 6]*j.cmp10;s12=0;                break;
+    	case "F37(1950)": am=j.am3[2];    ae=j.ae3[2];    as=0;           a32=j.aa3[ 3]*j.cep11;a12=j.aa1[ 3]*j.cmp11;s12=0; 
+    	}
+    	double[] dcof = {am,ae,as,a32,a12,s12}; String[] scof = new String[6];
+    	int n=0; for (double d : dcof) scof[n++] = String.valueOf(""+String.format("%.2f", d));
+    	setCofParameters(scof);
+    }
+    
+    public JButton getResetButton() {
+        JButton button = new JButton("Reset");
+        button.addActionListener(this); 
+        return button;
+    }
+
     public void setJanrTabNames(int i, String... names) {
         for(String name : names) detectorTabNames[i].add(name); 
         if(dgmActive) {dgm[i].setDetectorTabNames(names); setDetectorCanvas(i,dgm[i].getDetectorCanvas()); return;}
@@ -341,7 +393,7 @@ public class JanrMonitor implements ActionListener {
     }
     
     public void createHistos(int run) {
-        // initialize canvas and create histograms
+
     }
     
     public void plotHistos(int run, int nt, int bin) {
@@ -365,7 +417,10 @@ public class JanrMonitor implements ActionListener {
     }
     
     public void ResetAction() {
-    	
+    	hjanr_loadpar(3);
+    	hjanr_loadpar(2); 
+    	plotHistos(getRunNumber(),nt,bin); 
+    	Res2Amp(j.res_name[cof]);    	
     }
     
     public void resetEventListener() {
@@ -384,7 +439,7 @@ public class JanrMonitor implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) { 
-    	if(e.getActionCommand().compareTo("Reset")==0) {hjanr_loadpar(3);hjanr_loadpar(2); plotHistos(getRunNumber(),nt,bin); Res2Amp(j.res_name[cof]);}
+    	if(e.getActionCommand().compareTo("Reset")==0) ResetAction();
     	if(bG0!=null)  binWCF = Integer.parseInt(bG0.getSelection().getActionCommand()); 
         if(bG1!=null) bin1234 = Integer.parseInt(bG1.getSelection().getActionCommand());
         nt = bin1234 + 4*binWCF;
